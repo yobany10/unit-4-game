4143f40551e0249f9569a007d636e0d025328ff9
Yobany <yobanydesign@gmail.com>
2018-10-23 12:34:06 -0400

    Added character object in javascript and made changes to front-end

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    var characters = {
+        'Captain': {
+            name: "Captain",
+            health: 240,
+            attack: 16,
+            enemyAttackBack: 30
+        },
+        'Thanos': {
+            name: "Thanos",
+            health: 200,
+            attack: 28,
+            enemyAttackBack: 10
+        },
+        'Venom': {
+            name: "Venom",
+            health: 300,
+            attack: 16,
+            enemyAttackBack: 40
+        },
+        'Ultron': {
+            name: "Ultron",
+            health: 360,
+            attack: 14,
+            enemyAttackBack: 40,
+        }
+    }
+})
